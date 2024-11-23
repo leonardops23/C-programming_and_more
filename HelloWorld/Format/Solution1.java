34f8bdcfc93cbf6d560411ebcc6c3e1ebc7b547b
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-11-23 02:30:47 -0500

    first exercise and format out

---
+++ HelloWorld/Format/Solution1.java
@@ -0,0 +1,9 @@
+package HelloWorld.Format;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        // sts....(format, arg)
+        int age = 23;
+        System.out.printf("Age: %d%n", age);
+    }
+}
