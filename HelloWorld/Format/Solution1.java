a865be3b630826aecf8c7a471acd02ba65c15c08
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-12-20 00:54:28 -0500

    Solving problem

--- HelloWorld/Format/Solution1.java
+++
@@ -1,9 +0,0 @@
-package HelloWorld.Format;
-
-public class Solution1 {
-    public static void main(String[] args) {
-        // sts....(format, arg)
-        int age = 23;
-        System.out.printf("Age: %d%n", age);
-    }
-}
--- HelloWorld/Format/Solution2.java
+++
@@ -1,8 +0,0 @@
-package HelloWorld.Format;
-
-public class Solution2 {
-    public static void main(String[] args) {
-        String name = "Jose";
-        System.out.printf("%s%n leo", name);
-    }
-}
